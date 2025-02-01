73b96a50623bfddecfb61944fc00e13578fcab0d
Chris Lyons <52037738+mephmanx@users.noreply.github.com>
2025-02-01 12:26:15 -0500

    adding known_hosts entry
    Signed-off-by: Chris Lyons <52037738+mephmanx@users.noreply.github.com>

--- setup.sh
+++ setup.sh
@@ -7,4 +7,5 @@ terraform apply --auto-approve
 virsh list --all | grep running | awk '{print $2}' | while read vm_name; do
     IP=$(virsh domifaddr $vm_name --source agent | grep ens3 | awk '{print $4}' | cut -d "/" -f 1)
     sudo hostsed add $IP $vm_name
+    ssh-keyscan -H $vm_name >> ~/.ssh/known_hosts
 done
