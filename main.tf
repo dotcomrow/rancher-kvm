bf2bd627051f39d1b245961f7ecbc57759c0e304
Chris Lyons <52037738+mephmanx@users.noreply.github.com>
2025-02-01 10:40:30 -0500

    update
    Signed-off-by: Chris Lyons <52037738+mephmanx@users.noreply.github.com>

--- main.tf
+++ main.tf
@@ -287,7 +287,7 @@ resource "libvirt_domain" "srvr_node" {
   memory     = var.SRVR_NODE_MEMORY
   vcpu       = var.SRVR_NODE_VCPU
   autostart  = true
-  qemu_agent = true
+  qemu_agent = false
 
   cloudinit = libvirt_cloudinit_disk.cloudinit_srvr[count.index].id
 
@@ -335,7 +335,7 @@ resource "libvirt_domain" "etcd_node" {
   memory     = var.ETCD_NODE_MEMORY
   vcpu       = var.ETCD_NODE_VCPU
   autostart  = true
-  qemu_agent = true
+  qemu_agent = false
 
   cloudinit = libvirt_cloudinit_disk.cloudinit_etcd[count.index].id
 
@@ -383,7 +383,7 @@ resource "libvirt_domain" "ctrl_node" {
   memory     = var.CTRL_NODE_MEMORY
   vcpu       = var.CTRL_NODE_VCPU
   autostart  = true
-  qemu_agent = true
+  qemu_agent = false
 
   cloudinit = libvirt_cloudinit_disk.cloudinit_ctrl[count.index].id
 
@@ -431,7 +431,7 @@ resource "libvirt_domain" "work_node" {
   memory     = var.WORK_NODE_MEMORY
   vcpu       = var.WORK_NODE_VCPU
   autostart  = true
-  qemu_agent = true
+  qemu_agent = false
 
   cloudinit = libvirt_cloudinit_disk.cloudinit_work[count.index].id
 
