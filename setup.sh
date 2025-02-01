e64526ce21d8d5d1044e46125f269be027471d20
Chris Lyons <52037738+mephmanx@users.noreply.github.com>
2025-02-01 12:26:44 -0500

    removing known hosts
    Signed-off-by: Chris Lyons <52037738+mephmanx@users.noreply.github.com>

--- setup.sh
+++ setup.sh
@@ -3,6 +3,8 @@
 terraform init
 terraform apply --auto-approve
 
+rm -rf ~/.ssh/known_hosts
+
 # iterate over machines and add host entries to hosts file using qemu guest agent
 virsh list --all | grep running | awk '{print $2}' | while read vm_name; do
     IP=$(virsh domifaddr $vm_name --source agent | grep ens3 | awk '{print $4}' | cut -d "/" -f 1)
