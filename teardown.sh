05db65f1377abf90971f818c8e365be5f8e36d71
Chris Lyons <52037738+mephmanx@users.noreply.github.com>
2025-02-01 11:38:02 -0500

    adding host entries
    Signed-off-by: Chris Lyons <52037738+mephmanx@users.noreply.github.com>

--- teardown.sh
+++ teardown.sh
@@ -8,6 +8,11 @@ virsh list --all | grep running
 echo "Shutting down VMs..."
 virsh list --all | grep running | awk '{print $2}' | while read vm_name; do
     virsh shutdown $vm_name
+    
+    # remove host entries from hosts file
+    virsh domifaddr $vm_name --source agent | grep ipv4 | awk '{print $4}' | while read ip; do
+        sed -i "/$ip/d" /etc/hosts
+    done
 done
 
 # Wait for VMs to shutdown (optional)
@@ -19,3 +24,4 @@ done
 echo "All VMs shutdown."
 
 terraform destroy --auto-approve
+
