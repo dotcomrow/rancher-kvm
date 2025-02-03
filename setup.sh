ba40a4f07131e17bf82b25aa0d3a017d961130cc
Chris Lyons <52037738+mephmanx@users.noreply.github.com>
2025-02-02 20:14:37 -0500

    fix
    Signed-off-by: Chris Lyons <52037738+mephmanx@users.noreply.github.com>

--- setup.sh
+++ setup.sh
@@ -16,7 +16,7 @@ rm -rf ~/.ssh/known_hosts
 
 # iterate over machines and add host entries to hosts file using qemu guest agent
 virsh list --all | grep running | awk '{print $2}' | while read vm_name; do
-    while ! grep -q "ens" <(virsh domifaddr $vm_name --source agent 2>&1); do
+    while ! grep -q "ens3" <(virsh domifaddr $vm_name --source agent 2>&1); do
         sleep 1;
     done
     IP=$(virsh domifaddr $vm_name --source agent | grep ens3 | awk '{print $4}' | cut -d "/" -f 1);
