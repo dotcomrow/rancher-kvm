9a192a77e4feb15e5ba5044892bde0f8484ca834
Chris Lyons <52037738+mephmanx@users.noreply.github.com>
2025-02-05 10:14:17 -0500

    another update
    Signed-off-by: Chris Lyons <52037738+mephmanx@users.noreply.github.com>

--- setup.sh
+++ setup.sh
@@ -247,21 +247,18 @@ done < <(virsh list --all | awk '/running/ && $2 ~ /'"$SERVER_NODE_PATTERN"'/ {p
 # Step 2: Install RKE2 on ETCD Nodes
 echo "Setting up ETCD Nodes..."
 while IFS= read -r NODE; do
-    echo "Installing Rancher RKE on $NODE";
     install_rke2 "$NODE" "server";
 done < <(virsh list --all | awk '/running/ && $2 ~ /'"$ETCD_NODE_PATTERN"'/ {print $2}')
 
 # Step 3: Install RKE2 on Additional Control Plane Nodes
 echo "Setting up Control Plane Nodes..."
 while IFS= read -r NODE; do
-    echo "Installing Rancher RKE on $NODE";
     install_rke2 "$NODE" "server";
 done < <(virsh list --all | awk '/running/ && $2 ~ /'"$CONTROL_NODE_PATTERN"'/ {print $2}')
 
 # Step 4: Install RKE2 on Worker Nodes
 echo "Setting up Worker Nodes..."
 while IFS= read -r NODE; do
-    echo "Installing Rancher RKE on $NODE";
     install_rke2 "$NODE" "agent";
 done < <(virsh list --all | awk '/running/ && $2 ~ /'"$WORKER_NODE_PATTERN"'/ {print $2}')
 
