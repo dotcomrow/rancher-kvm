2f556163242a96c06e8cb3503cc93c6c278b518e
Chris Lyons <52037738+mephmanx@users.noreply.github.com>
2025-02-06 17:27:15 -0500

    update
    Signed-off-by: Chris Lyons <52037738+mephmanx@users.noreply.github.com>

--- main.tf
+++ main.tf
@@ -159,7 +159,7 @@ variable "WORK_VM_IMG_SIZE" {
 
 # instance the provider
 provider "libvirt" {
-  uri = "qemu:///session"
+  uri = "qemu+unix:///system?socket=/var/run/libvirt/libvirt-sock"
 }
 
 ################################################################################
