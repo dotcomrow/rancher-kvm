b99f2796216afdaa484ea93209ebeab913f3c705
Chris Lyons <52037738+mephmanx@users.noreply.github.com>
2025-02-10 10:22:43 -0500

    fix
    Signed-off-by: Chris Lyons <52037738+mephmanx@users.noreply.github.com>

--- main.tf
+++ main.tf
@@ -14,13 +14,8 @@ variable "VM_USER" {
   type    = string
 }
 
-variable "UBUNTU_VERSION" {
-  default = "24.04"
-  type    = string
-}
-
 variable "VM_IMG_PATH" {
-  default = "https://cloud-images.ubuntu.com/releases/${var.UBUNTU_VERSION}/release/ubuntu-${var.UBUNTU_VERSION}-server-cloudimg-amd64.img"
+  default = "https://cloud-images.ubuntu.com/releases/24.04/release/ubuntu-24.04-server-cloudimg-amd64.img"
   type    = string
 }
 
